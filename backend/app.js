c66ed7bfb11dbc3332afe3c12d241ca2eb1a7c4f
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-09-25 18:08:55 +0200

    update CORS

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-with, Content-Type, Accept, Z-Key');
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
