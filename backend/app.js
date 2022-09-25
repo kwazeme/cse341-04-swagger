638192f88e53e1e4cc28262ca29aae507d476e85
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-09-25 20:38:32 +0200

    add control access methods allowed

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,9 @@ app.use(bodyParser.json())
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-with, Content-Type, Accept, Z-Key');
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 })
   .use(bodyParser.urlencoded({ extended: true }))
