c2d7451c1f522120851aee6ee9925f800d5dfcbd
kwazeme <97014623+kwazeme@users.noreply.github.com>
2022-09-25 14:30:52 +0200

    initial coomit swagger

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+// Application initilzation
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoDB = require('./db/mongodb');
+
+// const swaggerAutogen = require('swagger-autogen')();
+const port = process.env.PORT || 8080;
+
+const professionalRoutes = require('./routes/professional');
+const contactRoutes = require('./routes/contacts');
+const docRoutes = require('./routes/swagger');
+const app = express();
+
+// Add swagger app.js
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+
+app.use(bodyParser.json())
+  .use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use('/professional', professionalRoutes)  
+  .use('/contacts', contactRoutes)
+
+  // .use('/api-docs', docRoutes);
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+mongoDB.initDb((err, mongoDB) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Server started on Port ${port}. open frontend live server to view professional \n`);
+    console.log(`ctrl+click http://localhost:${port}/professional`)
+    console.log(`ctrl+click http://localhost:${port}/contacts`)
+    console.log(`ctrl+click http://localhost:${port}/api-docs`)
+  }
+});
---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+// Application endpoints index
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+router.use('/professional', require('./professional'))
+
+router.use('/api-docs', require('./swagger'));
+
+module.exports = router;
